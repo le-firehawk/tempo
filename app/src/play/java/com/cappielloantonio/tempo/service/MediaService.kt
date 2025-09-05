d4cb6c5c9a6a08ed7ad7d57b584671828294c106
Jaime García <55400857+jaime-grj@users.noreply.github.com>
2025-09-06 00:20:35 +0200

    fix: Update MediaService in all build variants

--- app/src/play/java/com/cappielloantonio/tempo/service/MediaService.kt
+++ app/src/play/java/com/cappielloantonio/tempo/service/MediaService.kt
@@ -14,13 +14,13 @@ import androidx.media3.common.Tracks
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.DefaultLoadControl
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
 import androidx.media3.session.MediaLibraryService
 import androidx.media3.session.MediaSession.ControllerInfo
 import com.cappielloantonio.tempo.repository.AutomotiveRepository
 import com.cappielloantonio.tempo.ui.activity.MainActivity
 import com.cappielloantonio.tempo.util.Constants
 import com.cappielloantonio.tempo.util.DownloadUtil
+import com.cappielloantonio.tempo.util.DynamicMediaSourceFactory
 import com.cappielloantonio.tempo.util.Preferences
 import com.cappielloantonio.tempo.util.ReplayGainUtil
 import com.google.android.gms.cast.framework.CastContext
@@ -74,7 +74,7 @@ class MediaService : MediaLibraryService(), SessionAvailabilityListener {
     private fun initializePlayer() {
         player = ExoPlayer.Builder(this)
                 .setRenderersFactory(getRenderersFactory())
-                .setMediaSourceFactory(getMediaSourceFactory())
+                .setMediaSourceFactory(DynamicMediaSourceFactory(this))
                 .setAudioAttributes(AudioAttributes.DEFAULT, true)
                 .setHandleAudioBecomingNoisy(true)
                 .setWakeMode(C.WAKE_MODE_NETWORK)
@@ -215,9 +215,6 @@ class MediaService : MediaLibraryService(), SessionAvailabilityListener {
     }
 
     private fun getRenderersFactory() = DownloadUtil.buildRenderersFactory(this, false)
-
-    private fun getMediaSourceFactory() =
-            DefaultMediaSourceFactory(this).setDataSourceFactory(DownloadUtil.getDataSourceFactory(this))
 
     override fun onCastSessionAvailable() {
         setPlayer(player, castPlayer)
